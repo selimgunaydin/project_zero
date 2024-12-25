58e2cb8d52ba4426ea4dc9214c1eaa68b46623ef
selimgunaydinn <selim.gunaydin@akinon.com>
2024-12-25 13:02:09 +0300

    Implement user authentication with registration and login functionality

---
+++ app/models/User.ts
@@ -0,0 +1,28 @@
+import mongoose, { model, Schema } from "mongoose";
+import { UserDocument } from "../types";
+
+const UserSchema = new Schema<UserDocument>({
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Email is required"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Email is invalid",
+    ],
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: [true, "Name is required"]
+  }
+},
+{
+  timestamps: true,
+}
+);
+
+export const  User  =  mongoose.models?.User  ||  model<UserDocument>('User', UserSchema);
---
+++ app/types/index.ts
@@ -0,0 +1,10 @@
+export interface UserDocument {
+  _id: string;
+  email: string;
+  password: string;
+  name: string;
+  phone: string;
+  image: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
