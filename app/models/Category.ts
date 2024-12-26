3491e2b3fc5a907eb1164587519278d997ce58b5
selimgunaydinn <selim.gunaydin@akinon.com>
2024-12-26 14:06:07 +0300

    Enhance Category model; add description field and ensure required validation

--- app/models/Category.ts
+++ app/models/Category.ts
@@ -1,17 +1,20 @@
 import mongoose, { model, Schema } from "mongoose";
 import { CategoryDocument } from "../types";
 
-const CategorySchema = new Schema<CategoryDocument>({
-  name: {
-    type: String,
-    required: [true, "Name is required"],
+const CategorySchema = new Schema<CategoryDocument>(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+    },
   },
-  description: {
-    type: String,
-    required: [true, "Description is required"],
-  },
-}, {
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const Category = mongoose.models?.db_category || model<CategoryDocument>("db_category", CategorySchema);
