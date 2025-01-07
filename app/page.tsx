6a70a51410b0f6ae849165f2da2182bc99f389f2
Mehmet Türkan <mehmet.turkan@akinon.com>
2025-01-07 12:01:44 +0300

    MT | next-ui imported

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default async function Home() {
   return (
     <main>
-      <div className="w-full flex flex-col items-center justify-center py-4 min-h-screen">
+      <div className="w-full flex flex-col items-center justify-center py-4">
         <div>
           <h1 className="text-2xl font-bold text-center">
             Welcome to the Business Management System
--- app/providers/provider.tsx
+++ app/providers/provider.tsx
@@ -3,6 +3,7 @@
 import { SessionProvider } from "next-auth/react";
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "../store";
+import { NextUIProvider } from "@nextui-org/react";
 
 type Props = {
   children?: React.ReactNode;
@@ -11,7 +12,11 @@ type Props = {
 export const Provider = ({ children }: Props) => {
   return (
     <SessionProvider>
-      <ReduxProvider store={store}>{children}</ReduxProvider>
+      <ReduxProvider store={store}>
+        <NextUIProvider>
+          <div className="min-h-screen flex flex-col">{children}</div>
+        </NextUIProvider>
+      </ReduxProvider>
     </SessionProvider>
   );
 };
