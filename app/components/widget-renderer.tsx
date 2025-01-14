4b68ed48805a43522f7f60ca3856d42e53c57a81
selimgunaydinn <selim.gunaydin@akinon.com>
2025-01-14 19:48:58 +0300

    Remove unused widget components and their associated files to streamline the codebase

--- app/components/widget-renderer.tsx
+++ app/components/widget-renderer.tsx
@@ -1,13 +1,5 @@
 "use client";
 
-import HeroWidget from "../widgets/hero";
-import StatsWidget from "../widgets/stats";
-import FeatureWidget from "../widgets/feature";
-import TestimonialsWidget from "../widgets/testimonials";
-import PricingWidget from "../widgets/pricing";
-import BlockCarouselWidget from "../widgets/block-carousel";
-import NewsletterWidget from "../widgets/newsletter";
-
 interface Widget {
   _id: string;
   type: string;
@@ -18,33 +10,16 @@ interface Props {
   widgets: Widget[];
 }
 
-const widgetComponents = {
-  hero: HeroWidget,
-  stats: StatsWidget,
-  feature: FeatureWidget,
-  testimonials: TestimonialsWidget,
-  pricing: PricingWidget,
-  "block-carousel": BlockCarouselWidget,
-  newsletter: NewsletterWidget,
-};
-
 export default function WidgetRenderer({ widgets }: Props) {
   return (
     <>
       {widgets.map((widget) => {
-        const WidgetComponent =
-          widgetComponents[
-            widget.type.toLowerCase() as keyof typeof widgetComponents
-          ];
-        if (!WidgetComponent) {
-          return (
-            <div
-              key={widget._id}
-              dangerouslySetInnerHTML={{ __html: widget.data }}
-            ></div>
-          );
-        }
-        return <WidgetComponent key={widget._id} data={widget.data} />;
+        return (
+          <div
+            key={widget._id}
+            dangerouslySetInnerHTML={{ __html: widget.data }}
+          ></div>
+        );
       })}
     </>
   );
