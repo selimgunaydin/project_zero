79c52505dee52ac5335080a77357bc6362ea77d8
selimgunaydinn <selim.gunaydin@akinon.com>
2025-01-16 17:26:25 +0300

    Refactor Next.js configuration: Remove unused path alias and update image domain comments for clarity.

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
-import path from 'path'; 
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // İzin verilen alan adları
+    //cloudinary haricindekiler kaldırılacak.
     domains: ['res.cloudinary.com', 'tailwindui.com', 'static.vecteezy.com', 'images.unsplash.com'],
-  },
-  webpack: (config:any) => {
-    config.resolve.alias['generatedWidgets'] = path.resolve(__dirname, 'app/widgets/generated');
-    return config;
   },
 };
 
